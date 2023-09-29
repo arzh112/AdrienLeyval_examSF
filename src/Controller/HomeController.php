1d69593a3dd3c51600dc7c97659ba23406fe55f6
alv <adlv@test.com>
2023-09-29 19:35:22 +0200

    front page d'accueil, navbar, hommcontroller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function home(): Response
+    public function home(Security $security): Response
     {
+        $user = $security->getUser();
         
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'user' => $user,
         ]);
     }
 }
